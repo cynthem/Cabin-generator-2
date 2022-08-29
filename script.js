d1866e71004323560161674ee67f017268699a1b
doozles411 <cindy.phelps01@gmail.com>
2022-08-29 13:23:16 -0700

    Adjust region form event handling logic

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const dateBtn = document.querySelector('.date-choices');
 const dateCard = document.querySelector('.choose-date');
 const dateOverlay = document.querySelector('.overlay-choose-date');
+const regionBtn = document.querySelector('.region-choices');
+const regionCard = document.querySelector('.choose-region');
+const regionOverlay = document.querySelector('.overlay-choose-region');
 
 dateBtn.addEventListener('submit', e => {
     e.preventDefault();
     dateCard.classList.toggle('choose-date-open');
     dateOverlay.classList.toggle('overlay-date-invisible');
+    regionCard.classList.toggle('choose-region-open');
+    regionOverlay.classList.toggle('overlay-region-invisible');
 });
-
-const regionBtn = document.querySelector('.region-choices');
-const regionCard = document.querySelector('.choose-region');
-const regionOverlay = document.querySelector('.overlay-choose-region');
 
 regionBtn.addEventListener('submit', e => {
     e.preventDefault();
