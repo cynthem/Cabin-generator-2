0a8220401ba57c9924f325c56e394275cbce3175
doozles411 <cindy.phelps01@gmail.com>
2022-08-29 19:51:13 -0700

    Create southwest results rendering function

--- script.js
+++ script.js
@@ -180,4 +180,60 @@ function renderNorthwest(date) {
     }
 }
 
-function renderSoutheast(date) {}
+function renderSoutheast(date) {
+    if (date === 'oct14') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('oct14')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'oct21') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('oct21')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'oct28') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('oct28')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'nov4') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('nov4')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'nov11') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('nov11')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'nov18') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('nov18')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'nov25') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('nov25')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'dec9') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('dec9')) {
+                embed.classList.remove('hide');
+            }
+        });
+    } else if (date === 'dec16') {
+        embeds.forEach(embed => {
+            if (embed.classList.contains('southeast') && embed.classList.contains('dec16')) {
+                embed.classList.remove('hide');
+            }
+        });
+    }
+}
